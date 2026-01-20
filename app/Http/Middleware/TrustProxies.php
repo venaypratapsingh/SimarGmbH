e2ca313bd9bdd50eae01c5a0084dc6c307f60ac3
Vinay Pratap <venaypratapsingh@example.com>
2026-01-20 19:53:47 +0100

    Trust Render proxies for HTTPS detection

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -12,7 +12,7 @@ class TrustProxies extends Middleware
      *
      * @var array|string|null
      */
-    protected $proxies;
+    protected $proxies = '*'; // Trust all proxies (Render uses proxies)
 
     /**
      * The headers that should be used to detect proxies.
