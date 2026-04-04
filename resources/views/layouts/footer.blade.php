e2878752908847b0fc1caa5bfb234c0fbc954ab8
Vinay Pratap <venaypratapsingh@example.com>
2026-04-04 18:22:08 +0200

    Change footer credit from VENAY PRATAP SINGH to NIGHT NINJA

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,3 +1,3 @@
 <footer class="footer">
-     ©  {{date('Y')}} Attendance Management System <span class="d-none d-sm-inline-block"> - Crafted with <i class="mdi mdi-heart text-danger"></i> VENAY PRATAP SINGH</span>.
+     ©  {{date('Y')}} Attendance Management System <span class="d-none d-sm-inline-block"> - Crafted with <i class="mdi mdi-heart text-danger"></i> NIGHT NINJA</span>.
 </footer>
